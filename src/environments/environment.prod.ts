70c01ff46e8c4c5037a0e00380bf855b41d8d24d
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2019-09-21 14:13:49 +0200

    Mobile nav

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://dok-io.net/api',
-  WEB_HOST: 'http://dok-io.net'
+  API_URL: 'https://dok-io.net/api',
+  WEB_HOST: 'https://dok-io.net'
 };
